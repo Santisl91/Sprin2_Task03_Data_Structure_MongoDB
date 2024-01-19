fbdc50e95214b7dfb40a53f5e2875c381fe62d25
Santiago Santos <santiagosantoslapel@gmail.com>
2024-01-19 15:32:24 +0100

    S2T3N1 corregido

--- N1_Ex01/glasses.js
+++
@@ -1,5 +0,0 @@
-
-db.createCollection( 'glasses', {validator: {$jsonSchema: {bsonType: 'object',title:'glasses',required: [         'mark',          'frame_type',          'frame_color',          'price',          'prescription',          'lense_color',          'supplier',          'glasses'],properties: {mark: {bsonType: 'object',
-title:'object',required: [         'name'],properties: {name: {bsonType: 'string'}}},frame_type: {enum: },frame_color: {enum: },price: {bsonType: 'int'},prescription: {bsonType: 'object',
-title:'object',required: [         'rigth eye',          'left eye'],properties: {rigth eye: {bsonType: 'decimal'},left eye: {bsonType: 'decimal'}}},lense_color: {bsonType: 'object',
-title:'object',required: [         'rigth eye',          'left eye'],properties: {rigth eye: {bsonType: 'string'},left eye: {bsonType: 'string'}}},supplier: {bsonType: 'objectId'},glasses: {bsonType: 'objectId'}}         }      }});  
